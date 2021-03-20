f5ec1cf31555f98510f192ed21f08f85e27eba3c
Zack Eckersley Pallett <zackary12389@yahoo.co.uk>
2021-03-20 17:38:40 +0000

    Added Planned Closure to getDistruptionColor

--- src/routes/tube/tubeStatus.svelte
+++ src/routes/tube/tubeStatus.svelte
@@ -19,13 +19,21 @@
         if (str === 'Good Service') return '#47b300';
         if (str === 'Minor Delays') return '#ffae00';
         if (str === 'Reduced Service' || str === 'Part Suspended') return '#db6300';
-        if (str === 'Planned Closure' || str === 'Severe Delays') return '#db2c00';
+        if (str === 'Planned Closure' || str === 'Severe Delays' || str === 'Part Closure') return '#db2c00';
         return '#424242';
     }
 
     onMount(() => {
         getAllLineStatuses();
-        return () => {};
+
+        /* Update the status data */
+        const dataInterval = setInterval(() => {
+            getAllLineStatuses();
+        }, 300000);
+
+        return () => {
+            clearInterval(dataInterval);
+        };
     });
 </script>
 
