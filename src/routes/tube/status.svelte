e8ccc47e3f356d8a326875276399d5bc297a00f3
Zack Eckersley Pallett <zackary12389@yahoo.co.uk>
2021-03-21 12:11:30 +0000

    Added Service Closed to getDistruptionColor

--- src/routes/tube/status.svelte
+++ src/routes/tube/status.svelte
@@ -19,7 +19,7 @@
         if (str === 'Good Service') return '#47b300';
         if (str === 'Minor Delays') return '#ffae00';
         if (str === 'Reduced Service' || str === 'Part Suspended') return '#db6300';
-        if (str === 'Planned Closure' || str === 'Severe Delays' || str === 'Part Closure') return '#db2c00';
+        if (str === 'Planned Closure' || str === 'Severe Delays' || str === 'Part Closure' || str === 'Service Closed') return '#db2c00';
         return '#424242';
     }
 
