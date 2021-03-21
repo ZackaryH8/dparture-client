c19a5ad8ada609721c240cae0e6046396b15c6f8
Zack Eckersley Pallett <zackary12389@yahoo.co.uk>
2021-03-21 13:32:50 +0000

    Fixed text color for line status

--- src/routes/tube/status.svelte
+++ src/routes/tube/status.svelte
@@ -43,8 +43,8 @@
             <table>
                 {#each statuses as line}
                     <tr>
-                        <td style="color:{getLineTextColor(line.id || '')}; background-color: {getLineBGColor(line.id || '')}">
-                            <h4>{line.name}</h4>
+                        <td style="background-color: {getLineBGColor(line.id || '')}">
+                            <h4 style="color: {getLineTextColor(line.id || '')};">{line.name}</h4>
                         </td>
                         <td class="distruption" style="background: linear-gradient(90deg, #424242 96%, {getDistruptionColor(line.lineStatuses[0].statusSeverityDescription)} 0);">
                             {line.lineStatuses[0].statusSeverityDescription}
