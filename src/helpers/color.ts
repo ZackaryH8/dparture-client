28b1949bfbf23a8d402a9e782b04943209ab6685
ZackaryH8 <zackary12389@yahoo.co.uk>
2021-03-25 09:32:12 +0000

    Normalize function declarations

--- src/helpers/color.ts
+++ src/helpers/color.ts
@@ -1,10 +1,18 @@
 import { colors } from '../assets/data/tflColors';
 
-export const getLineBGColor = (name: string): string => {
+export function getLineBGColor(name: string): string {
     return colors.find((color: any) => color.name === name)?.hex;
 };
 
-export const getLineTextColor = (name: string): string => {
+export function getLineTextColor(name: string): string {
     if (name === 'circle' || name === 'hammersmith-city' || name === 'waterloo-city') return '#282828';
     return '#fff';
 };
+
+export function getDistruptionColor(str: string = ''): string {
+    if (str === 'Good Service') return '#47b300';
+    if (str === 'Minor Delays') return '#ffae00';
+    if (str === 'Reduced Service' || str === 'Part Suspended') return '#db6300';
+    if (str === 'Planned Closure' || str === 'Severe Delays' || str === 'Part Closure' || str === 'Service Closed') return '#db2c00';
+    return '#424242';
+}
