43cdd80cf191b0ae88e0936db004e1aafdffc16f
Zack Eckersley Pallett <zackary12389@yahoo.co.uk>
2021-03-28 15:57:52 +0100

    Added suspended

--- src/helpers/color.ts
+++ src/helpers/color.ts
@@ -13,6 +13,6 @@ export function getDistruptionColor(str: string = ''): string {
     if (str === 'Good Service') return '#47b300';
     if (str === 'Minor Delays') return '#ffae00';
     if (str === 'Reduced Service' || str === 'Part Suspended') return '#db6300';
-    if (str === 'Planned Closure' || str === 'Severe Delays' || str === 'Part Closure' || str === 'Service Closed') return '#db2c00';
+    if (str === 'Planned Closure' || str === 'Severe Delays' || str === 'Part Closure' || str === 'Service Closed' || str === "Suspended") return '#db2c00';
     return '#424242';
 }
