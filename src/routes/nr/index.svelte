99927c365e761a0ab6955ec727b381f7354545de
Zack Eckersley Pallett <zackary12389@yahoo.co.uk>
2021-04-19 22:15:35 +0100

    Swap logo to DpartureLogo component

--- src/routes/nr/index.svelte
+++ src/routes/nr/index.svelte
@@ -1,8 +1,6 @@
 <main>
     <div id="main-container">
-        <object data="img/logos/dparture/dparture_subtext_transparent.svg" type="image/svg+xml" width="350px" height="150px" title="dparture Logo">
-            <img src="img/logos/dparture.png" alt="dparture Logo" />
-        </object>
+        <DpartureLogo />
 
         <div class="split">
             <a class="button" href="#/nr/departures">
@@ -25,69 +23,70 @@
 </main>
 
 <script lang="ts">
+    import DpartureLogo from '../../components/common/dpartureLogo.svelte';
 </script>
 
 <style lang="less">
-@import '../../assets/css/colors.less';
-@import '../../assets/css/tfl.less';
+    @import '../../assets/css/colors.less';
+    @import '../../assets/css/tfl.less';
 
-* {
-    color: #fff;
-    margin-block-start: 0;
-    margin-block-end: 0;
-}
+    * {
+        color: #fff;
+        margin-block-start: 0;
+        margin-block-end: 0;
+    }
 
-main {
-    height: calc(100% - 40px);
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-}
-
-#main-container {
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    width: 450px;
-    height: 340px;
-    padding: 5px;
-    border-radius: 5px;
-    background-color: #333333;
-
-    .split {
-        width: 100%;
-        padding-top: 5px;
+    main {
+        height: calc(100% - 40px);
         display: flex;
-        justify-content: space-evenly;
-
-        img {
-            height: 100px;
-        }
-
-        p {
-            text-align: center;
-            margin-top: 5px;
-        }
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
     }
-}
 
-.button {
-    padding: 10px;
-    border-radius: 5px;
-    text-decoration: none;
-    cursor: pointer;
-}
-
-.button:hover {
-    text-decoration: none;
-    background-color: #424242;
-}
-
-@media only screen and (max-width: 600px) {
     #main-container {
-        width: 95%;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        width: 450px;
+        height: 340px;
+        padding: 5px;
+        border-radius: 5px;
+        background-color: #333333;
+
+        .split {
+            width: 100%;
+            padding-top: 5px;
+            display: flex;
+            justify-content: space-evenly;
+
+            img {
+                height: 100px;
+            }
+
+            p {
+                text-align: center;
+                margin-top: 5px;
+            }
+        }
     }
-}
+
+    .button {
+        padding: 10px;
+        border-radius: 5px;
+        text-decoration: none;
+        cursor: pointer;
+    }
+
+    .button:hover {
+        text-decoration: none;
+        background-color: #424242;
+    }
+
+    @media only screen and (max-width: 600px) {
+        #main-container {
+            width: 95%;
+        }
+    }
 </style>
